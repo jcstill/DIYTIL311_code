40d00e16a6eb8ec85ec401315e7d2608a0fbd9df
jcstill <31301328+jcstill@users.noreply.github.com>
2019-07-11 19:11:28 -0700

    ISP/Tester Code & Bootloader Files- All code needs tesing

---
+++ bootloader/bootloader.h
@@ -0,0 +1,23 @@
+/*
+*	Firmware for the programmable replica of the TIL311
+*	This file is a part of the DIYTIL311 source code.
+*
+*	Copyright (C) 2019  Jacob Still and Alex Golubow
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*	Project page: https://hackaday.io/project/166086-diytil311
+*	Code: https://github.com/jcstill/DIYTIL311
+*	
+*/
